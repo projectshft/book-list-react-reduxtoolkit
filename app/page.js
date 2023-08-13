37dbcbcade5c1f4f8bf153897ab306f0373dd792
Brian Jenney <brianjenney83@gmail.com>
2023-08-12 18:36:09 -0700

    working version

--- app/page.js
+++ app/page.js
@@ -1,48 +1,12 @@
-'use client';
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
-
-const counterSlice = createSlice({
-	name: 'counter',
-	initialState: 0,
-	reducers: {
-		increment: (state) => state + 1,
-		decrement: (state) => state - 1,
-		reset: () => 0,
-	},
-});
-
-const store = configureStore({
-	reducer: counterSlice.reducer,
-});
+import BookList from './components/BookList';
+import BookDetail from './components/BookDetail';
 
 export default function Home() {
-	const [count, setCount] = useState(store.getState());
-
-	useEffect(() => {
-		const unsubscribe = store.subscribe(() => {
-			setCount(store.getState());
-		});
-
-		return () => {
-			unsubscribe();
-		};
-	}, []);
-
-	const handleIncrement = () => {
-		store.dispatch(counterSlice.actions.increment());
-	};
-
-	const handleDecrement = () => {
-		store.dispatch(counterSlice.actions.decrement());
-	};
-
 	return (
 		<main className={styles.main}>
-			<button onClick={handleDecrement}>-</button>
-			<h1>{count}</h1>
-			<button onClick={handleIncrement}>+</button>
+			<BookList />
+			<BookDetail />
 		</main>
 	);
 }
