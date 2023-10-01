0f67686a4e47d45c3038b2cf1c9da0f4400360a5
Brian Jenney <brianjenney83@gmail.com>
2023-09-30 17:35:07 -0700

    solution to form validation

--- app/posts/new/page.js
+++ app/posts/new/page.js
@@ -3,56 +3,90 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
 import { createPost } from '../../store/slices/posts';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 const PostsNew = () => {
+	const schema = yup
+		.object({
+			title: yup.string().required().max(10),
+			categories: yup
+				.array()
+				.transform(function (value) {
+					return value.split(' ');
+				})
+				.required()
+				.min(1)
+				.max(5),
+			content: yup.string().required().min(120),
+		})
+		.required();
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
 	const dispatch = useDispatch();
 	const router = useRouter();
 	const [title, setTitle] = useState('');
 	const [categories, setCategories] = useState('');
 	const [content, setContent] = useState('');
 
-	const handleFormSubmit = (e) => {
-		e.preventDefault();
-		const id = Math.floor(Math.random() * 1000000000);
-		dispatch(
-			createPost({
-				title,
-				categories: categories.split(','),
-				content,
-				id,
-			})
-		);
+	const handleFormSubmit = (data) => {
+		dispatch(createPost({ id: Math.round(Math.random() * 100), ...data }));
 		router.push('/');
 	};
 
 	return (
-		<form onSubmit={handleFormSubmit}>
+		<form onSubmit={handleSubmit(handleFormSubmit)}>
 			<div className='form-group'>
 				<label>Title for Post</label>
 				<input
+					{...register('title', { required: true })}
 					className='form-control'
 					value={title}
 					onChange={(e) => setTitle(e.target.value)}
 				></input>
+				{errors.title?.message && (
+					<div className='alert alert-danger'>
+						{errors.title?.message}
+					</div>
+				)}
 			</div>
 
 			<div className='form-group'>
 				<label>Categories</label>
 				<input
+					{...register('categories', { required: true })}
 					className='form-control'
 					value={categories}
 					onChange={(e) => setCategories(e.target.value)}
 				></input>
+				{errors.categories?.message && (
+					<div className='alert alert-danger'>
+						{errors.categories?.message}
+					</div>
+				)}
 			</div>
 
 			<div className='form-group'>
 				<label>Post Content</label>
 				<textarea
+					{...register('content', { required: true })}
 					type='text-area'
 					className='form-control'
 					value={content}
 					onChange={(e) => setContent(e.target.value)}
 				></textarea>
+				{errors.content?.message && (
+					<div className='alert alert-danger'>
+						{errors.content?.message}
+					</div>
+				)}
 			</div>
 
 			<button className='btn btn-primary' type='submit'>
